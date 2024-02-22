273779200b548450dd8b2eced9006a9aa6ba83a1
91639 <91639@RSC>
2024-02-22 20:14:40 +0530

    Add project files.

---
+++ Mygame/Platform_common.cpp
@@ -0,0 +1,19 @@
+struct Button_State {
+	bool is_down;
+	bool changed;
+};
+
+enum {
+	BUTTON_UP,
+	BUTTON_DOWN,
+	BUTTON_W,
+	BUTTON_S,
+	BUTTON_LEFT,
+	BUTTON_RIGHT,
+	BUTTON_ENTER,
+	BUTTON_COUNT, 
+};
+
+struct Input {
+	Button_State buttons[BUTTON_COUNT];
+};
